4b416df080903ac4417f9bdff6147ca9660ac25e
Jekov-Evgen <jena1245481te@gmail.com>
2024-10-03 23:16:39 +0300

    Базовая настройка GUI

--- Python GUI/main_window.py
+++ Python GUI/main_window.py
@@ -6,17 +6,34 @@ class MainWindow:
         self.window = QtWidgets.QWidget()
         self.window.setWindowTitle("Мониторинг системы")
         
-        v_box = QtWidgets.QVBoxLayout()
-        h_box = QtWidgets.QHBoxLayout()
+        box_h_cpu = QtWidgets.QHBoxLayout()
+        box_h_memory = QtWidgets.QHBoxLayout()
+        box_h_video_card = QtWidgets.QHBoxLayout()
+        box_v = QtWidgets.QVBoxLayout()
         
-        cpu = QtWidgets.QLabel("Процессор")
+        cpu = QtWidgets.QLabel("Процессор: ")
         info_cpu = QtWidgets.QLabel("0%")
         
-        memory = QtWidgets.QLabel("Память")
+        memory = QtWidgets.QLabel("Память: ")
         info_memory = QtWidgets.QLabel("0%")
         
-        video_card = QtWidgets.QLabel("Видеокарта")
+        video_card = QtWidgets.QLabel("Видеокарта: ")
         info_video_card = QtWidgets.QLabel("0%")
+        
+        box_h_cpu.addWidget(cpu)
+        box_h_cpu.addWidget(info_cpu)
+        
+        box_h_memory.addWidget(memory)
+        box_h_memory.addWidget(info_memory)
+        
+        box_h_video_card.addWidget(video_card)
+        box_h_video_card.addWidget(info_video_card)
+        
+        box_v.addLayout(box_h_cpu)
+        box_v.addLayout(box_h_memory)
+        box_v.addLayout(box_h_video_card)
+        
+        self.window.setLayout(box_v)
         
     def run(self):
         self.window.show()
