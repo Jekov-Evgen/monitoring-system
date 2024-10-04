9547db2caf3b5d40785902caf3213754f8dc0d57
Jekov-Evgen <jena1245481te@gmail.com>
2024-10-04 22:40:45 +0300

    Добавлена память и GPU

--- Python_APP/Python_GUI/main_window.py
+++ Python_APP/Python_GUI/main_window.py
@@ -1,7 +1,7 @@
 from PySide6 import QtWidgets
 from PySide6 import QtCore
 from Python_GUI.style_app import  STYLE_WINDOW, STYLE_NAME, STYLE_INFO, STYLE_GREETINGS
-from Python_INFO.transfer_of_information import CPU
+from Python_INFO.transfer_of_information import CPU, Memory, GPU
 from time import sleep
 
 class MainWindow:
@@ -22,19 +22,19 @@ class MainWindow:
         self.info_cpu = QtWidgets.QLabel("0%")
         
         memory = QtWidgets.QLabel("Память: ")
-        info_memory = QtWidgets.QLabel("0%")
+        self.info_memory = QtWidgets.QLabel("0%")
         
         video_card = QtWidgets.QLabel("Видеокарта: ")
-        info_video_card = QtWidgets.QLabel("0")
+        self.info_video_card = QtWidgets.QLabel("0")
         
         box_h_cpu.addWidget(cpu)
         box_h_cpu.addWidget(self.info_cpu)
         
         box_h_memory.addWidget(memory)
-        box_h_memory.addWidget(info_memory)
+        box_h_memory.addWidget(self.info_memory)
         
         box_h_video_card.addWidget(video_card)
-        box_h_video_card.addWidget(info_video_card)
+        box_h_video_card.addWidget(self.info_video_card)
         
         box_h_cpu.setSpacing(15)
         box_h_memory.setSpacing(15)
@@ -57,8 +57,8 @@ class MainWindow:
         video_card.setStyleSheet(STYLE_NAME)
         
         self.info_cpu.setStyleSheet(STYLE_INFO)
-        info_memory.setStyleSheet(STYLE_INFO)
-        info_video_card.setStyleSheet(STYLE_INFO)
+        self.info_memory.setStyleSheet(STYLE_INFO)
+        self.info_video_card.setStyleSheet(STYLE_INFO)
         
         self.set_info()
         
@@ -70,9 +70,20 @@ class MainWindow:
         
     def set_info(self):
         self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.update_info)
+        self.timer.timeout.connect(self.update_info_CPU)
+        self.timer.timeout.connect(self.update_info_Memory)
+        self.timer.timeout.connect(self.update_info_GPU)
         self.timer.start(2000)
     
-    def update_info(self):
+    def update_info_CPU(self):
         info_CPU = CPU()
         self.info_cpu.setText(f"{info_CPU.info_CPU()}%")
+        
+    def update_info_Memory(self):
+        info_memory = Memory()
+        self.info_memory.setText(f"{info_memory.info_Memory()}%")
+        
+    def update_info_GPU(self):
+        info_GPU = GPU()
+        self.info_video_card.setText(f"{info_GPU.info_GPU()}%")
+        
--- Python_APP/Python_INFO/transfer_of_information.py
+++ Python_APP/Python_INFO/transfer_of_information.py
@@ -1,4 +1,5 @@
 import psutil
+import GPUtil
 
 class CPU:
     def __init__(self) -> None:
@@ -7,3 +8,38 @@ class CPU:
     def info_CPU(self) -> float:
         info_cpu = psutil.cpu_percent(interval=2)
         return info_cpu
+    
+class Memory:
+    def __init__(self) -> None:
+        pass
+    
+    def info_Memory(self):
+        info_memory = psutil.virtual_memory()
+        return info_memory[2]
+    
+    
+class GPU:
+    def __init__(self) -> None:
+        pass
+    
+    def info_GPU(self):
+        info_GPU = GPUtil.getGPUs()
+        self.Error = "GPU не обнаружен"
+        success = info_GPU        
+        info_list = []
+        
+        if not info_GPU:
+            return self.Error
+        else:           
+            for i, gpu in enumerate(info_GPU):
+                info_list.append(f"\nGPU {i + 1} Information:")
+                info_list.append(f"ID: {gpu.id}")
+                info_list.append(f"Name: {gpu.name}")
+                info_list.append(f"Driver: {gpu.driver}")
+                info_list.append(f"GPU Memory Total: {gpu.memoryTotal} MB")
+                info_list.append(f"GPU Memory Free: {gpu.memoryFree} MB")
+                info_list.append(f"GPU Memory Used: {gpu.memoryUsed} MB")
+                info_list.append(f"{gpu.load * 100}%")
+                info_list.append(f"GPU Temperature: {gpu.temperature}°C")
+                
+            return info_list[7]
